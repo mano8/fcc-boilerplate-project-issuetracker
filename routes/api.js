a81364696b15b520fac5c6767a5b8618934ad82d
Eli <eli.serra173@gmail.com>
2023-10-30 18:23:15 +0100

    Remove optional empty values from post result

--- routes/api.js
+++ routes/api.js
@@ -23,7 +23,7 @@ module.exports = function (app) {
       const issueItem = req.body;
       Issue.AddIssue(projectSlug, issueItem)
         .then(savedIssue=>{
-          res.json(Ut.filterObjectByKey(savedIssue.toObject(), Issue.getIssueKeys()))
+          res.json(Ut.filterObjectByKey(savedIssue.toObject(), Issue.getIssueKeys(), true))
         })
         .catch(err=> {
           res.json({ error: 'required field(s) missing' })
